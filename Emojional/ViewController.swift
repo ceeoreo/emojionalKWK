75043eac639905c08aff7f3162e98e83f71e220c
Ceora Ford <ceoraallthetime@gmail.com>
2019-07-17 21:34:57 -0400

    Complete Iteration 1

--- Emojional/ViewController.swift
+++ Emojional/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func showMessage(_ sender: UIButton) {
+        let alertController = UIAlertController(title: "Don't Stay Cold", message: "Warm up with some hot cocoa.", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "Okay", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+        //when user clicks emoji button
+        //alert displays saying "Warm up with some hot cocoa" etc
+        //alert has button user can press to get rid of alert
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
