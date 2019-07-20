d7dc2c03310d4fc76ac24a65d675f76dc87ce8b8
Ceora Ford <ceoraallthetime@gmail.com>
2019-07-20 01:57:08 -0400

    Complete Iteration 3

--- Emojional/ViewController.swift
+++ Emojional/ViewController.swift
@@ -9,11 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var emojis = ["🥵": "super heated", "🥶": "freezy face"]
+    var customMessages = [
+            "super heated" : ["Cool down with some water", "Turn on the AC", "Have an ice cream"],
+            "freezy face" : ["Warm up with some hot cocoa", "Wrap yourself in a blanket", "Turn on the heat"]
+    ]
+    let emojis = ["🥵": "super heated", "🥶": "freezy face"]
 
     @IBAction func showMessage(_ sender: UIButton) {
-        let emotionSelector = sender.titleLabel?.text
-        let alertController = UIAlertController(title: "Welcome", message: emojis["🥵"], preferredStyle: UIAlertController.Style.alert)
+        let selectedEmotion = sender.titleLabel?.text
+        let randomNumber = Int.random(in:  0 ... 2)
+        let emojiMessage = customMessages[emojis[selectedEmotion!]!]?[randomNumber]
+        let alertController = UIAlertController(title: "Welcome", message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
         alertController.addAction(UIAlertAction(title: "Okay", style: UIAlertAction.Style.default, handler: nil))
         present(alertController, animated: true, completion: nil)
         //when user clicks emoji button
